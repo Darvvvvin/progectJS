b1fd36ede6aafa7c3ed43e5b5150b85cef88a3cd
Ted <darvvvvin@gmail.com>
2020-12-30 16:09:18 +0200

    First skripts

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-const answer = +prompt('Are you 18?', '34');
-console.log(answer + 5);
+const numberOfFilmes = prompt("How many films do you watch?", '');
+
+const personalMovieDB = {
+    count: numberOfFilmes,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("One of the last watched film?", ""),
+      b = prompt('How much do you like this film?', ''),
+      c = prompt('One of the last watched film?', ''),
+      d = prompt('How much do you like this film?', ''); 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
